f9ed208d38ac88eab5b6e64cf5abfdee2f85c2d0
From HL105 <from_hl105@hl105.com>
2013-09-09 00:01:49 -0400

    jiejf

--- join-one-not-other.ps1
+++ join-one-not-other.ps1
@@ -1,8 +1,11 @@
 param(
 $file1,
 $file2,
+$outFile,
 $delim = '\|'
 )
+
+. ((Split-Path $script:MyInvocation.MyCommand.Path) + "/libConversion.ps1")
 
 function Make-Hash($file) {
     $recs = get-content $file
@@ -17,12 +20,13 @@ function Make-Hash($file) {
 function First-Not-Second($h1, $h2) {
     foreach ($key in $h1.Keys) {
         if (-not ($h2.ContainsKey($key))) {
-            write-host $key
+            CF-write-file $outFile $key
         }
     }
 }
     
 function Main() {
+    rm $outFile 2>&1 > $null
     $f1_hash = make-hash $file1
     $f2_hash = make-hash $file2
     First-Not-Second $f1_hash $f2_hash
