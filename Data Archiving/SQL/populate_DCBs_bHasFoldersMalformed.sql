d9a3b4102d4c96dee73816df8cea0fa3549b1992
Joe Hudson <Joe from Lexis Laptop>
2013-10-01 21:17:28 -0700

    ii

---
+++ Data Archiving/SQL/populate_DCBs_bHasFoldersMalformed.sql
@@ -0,0 +1,74 @@
+ --********************************************************************************
+ -- *** DCBs . bHasFoldersMalformed
+ -- Init
+ IF EXISTS(select * from sys.columns where name = 'bHasFoldersMalformed' and object_id=object_id('DCBs'))
+	BEGIN
+		DECLARE @SQL varchar(max);
+		SET @SQL= 'UPDATE DCBs SET bHasFoldersMalformed = null'
+		EXEC(@SQL)
+		print 1
+	END
+ELSE
+	BEGIN	
+		ALTER TABLE DCBs ADD bHasFoldersMalformed bit
+		print 0
+	END
+	
+-- TEST DATA
+insert into DCBs (dbid) values (10001), (10002),(10003),(10004)
+insert into Folders	(DBID, bExists) values 
+(10001,0),
+(10002,-1),
+(10003,1),
+(10004,0),(10004,1)
+
+ -- Compute
+DECLARE @dbid int;
+DECLARE @clientmatter varchar(25);
+DECLARE @hasMissing BIT;
+DECLARE @cmNum int;
+DECLARE @myDate as varchar(30);
+DECLARE mycursor CURSOR FOR
+  SELECT dbid 
+  FROM DCBs
+
+
+OPEN mycursor;
+
+FETCH NEXT FROM mycursor INTO @dbid
+set @cmNum = 0
+WHILE @@FETCH_STATUS = 0
+  BEGIN
+      SET @cmNum = @cmNum + 1
+      SET @myDate = convert(varchar, GETDATE()) + ' ' + convert(varchar, @cmnum) 
+      SET @myDate = @myDate + ' ' + convert(varchar, @dbid); 
+      RAISERROR( @mydate ,0,1) WITH NOWAIT;
+      
+      IF EXISTS (
+		SELECT * FROM Folders 
+		WHERE DBID = @dbid
+		  AND bExists = -1
+	  ) 		  
+        SET @hasMissing = 1;
+      ELSE
+        SET @hasMissing = 0;
+
+      UPDATE DCBs
+      SET    bHasFoldersMalformed = @hasMissing
+      WHERE  dbid = @dbid
+
+      FETCH NEXT FROM mycursor INTO @dbid
+  END
+CLOSE mycursor
+DEALLOCATE mycursor
+
+---- TEST
+select dbid,bHasFoldersMalformed from DCBs where dbid > 10000
+select dbid,bexists from Folders where DBID > 10000
+delete from DCBs where dbid > 10000
+delete from folders where dbid > 10000
+
+-- Report
+SELECT COUNT(*) AS 'DCBs.num HasFoldersMissing' 
+FROM DCBs 
+WHERE bHasFoldersMalformed = 1
