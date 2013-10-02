4efaa129bc1f0f4649bedaa563b6cb91a9cd5fb8
From HL105 <from_hl105@hl105.com>
2013-10-02 00:40:21 -0400

    ij

--- Data Archiving/SQL/populate_DCBs_bHasFoldersMalformed.sql
+++ Data Archiving/SQL/populate_DCBs_bHasFoldersMalformed.sql
@@ -1,26 +1,19 @@
  --********************************************************************************
  -- *** DCBs . bHasFoldersMalformed
  -- Init
- IF EXISTS(select * from sys.columns where name = 'bHasFoldersMalformed' and object_id=object_id('DCBs'))
-	BEGIN
-		DECLARE @SQL varchar(max);
-		SET @SQL= 'UPDATE DCBs SET bHasFoldersMalformed = null'
-		EXEC(@SQL)
-		print 1
-	END
-ELSE
-	BEGIN	
-		ALTER TABLE DCBs ADD bHasFoldersMalformed bit
-		print 0
-	END
+IF EXISTS(select * from sys.columns where name = 'bHasFoldersMalformed' and object_id=object_id('DCBs'))
+	ALTER TABLE DCBs DROP COLUMN bHasFoldersMalformed
+GO
+ALTER TABLE DCBs ADD bHasFoldersMalformed bit
+GO
 	
--- TEST DATA
-insert into DCBs (dbid) values (10001), (10002),(10003),(10004)
-insert into Folders	(DBID, bExists) values 
-(10001,0),
-(10002,-1),
-(10003,1),
-(10004,0),(10004,1)
+---- TEST DATA
+--insert into DCBs (dbid) values (10001), (10002),(10003),(10004)
+--insert into Folders	(DBID, bExists) values 
+--(10001,0),
+--(10002,-1),
+--(10003,1),
+--(10004,0),(10004,1)
 
  -- Compute
 DECLARE @dbid int;
@@ -63,10 +56,10 @@ CLOSE mycursor
 DEALLOCATE mycursor
 
 ---- TEST
-select dbid,bHasFoldersMalformed from DCBs where dbid > 10000
-select dbid,bexists from Folders where DBID > 10000
-delete from DCBs where dbid > 10000
-delete from folders where dbid > 10000
+--select dbid,bHasFoldersMalformed from DCBs where dbid > 10000
+--select dbid,bexists from Folders where DBID > 10000
+--delete from DCBs where dbid > 10000
+--delete from folders where dbid > 10000
 
 -- Report
 SELECT COUNT(*) AS 'DCBs.num HasFoldersMissing' 
