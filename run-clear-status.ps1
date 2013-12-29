c8ab6375ca0a1dac9c2710000ac9b60f74265a73
Joe Hudson <Joe from Lexis Laptop>
2013-12-29 11:56:45 -0800

    added run-qc-dict-pick-qc-words.ps1

---
+++ run-clear-status.ps1
@@ -0,0 +1,212 @@
+<#
+SYNOPSIS 
+
+.DESCRIPTION
+
+.PARAMETER Name
+
+.PARAMETER Extension
+
+.INPUTS
+None. You cannot pipe objects to this script
+
+.OUTPUTS
+
+.EXAMPLE
+One or more examples
+
+.EXAMPLE
+
+.LINK
+
+.LINK
+
+#>
+
+param(
+    $BatchID,
+    $ignoreStatus = $false,
+    $DriverFile,
+    [Parameter(mandatory=$true)]
+    [string] $FileStub,
+    [switch]$NotJustErrors,
+    [switch]$pgmBackup,
+    [switch]$pgmNatives,
+    [switch]$pgmImages,
+    [switch]$pgmImages2,
+    [switch]$pgmSizesAll,
+    [switch]$pgmFoldersNatives,
+    [switch]$pgmFoldersImages,
+    [switch]$pgmQcV8Tags,
+    [switch]$pgmQcListDictV8,
+    [switch]$pgmQcQueryDictV8,
+    [switch]$pgmQcPickWords,
+    [switch]$pgmConvDcb,
+    [switch]$pgmQcV10Tags,
+    [switch]$pgmQcListDictV10,
+    [switch]$pgmQcQueryDictV10,
+    [switch]$pgmQcCompareTags,
+    [switch]$incBlankStatus,
+    $startRow,
+    $endRow
+)
+
+
+. ((Split-Path $script:MyInvocation.MyCommand.Path) + "/libConversion.ps1")
+
+
+function Build-List-Of-Pgms() {
+    $pgms = @();
+    if ($pgmBackup) { $pgms += "backup-for-conversion"; }
+    if ($pgmNatives) { $pgms += "run-get-natives"; }
+    if ($pgmImages) { $pgms += "run-get-images"; }
+    if ($pgmImages2) { $pgms += "run-get-images2"; }
+    if ($pgmFoldersNatives) { $pgms += "run-get-natives-folders"; }
+    if ($pgmFoldersImages) { $pgms += "run-get-images-folders"; }
+    if ($pgmQcV8Tags) { $pgms += "run-qc-v8-tags"; }
+    if ($pgmQcListDictV8) { $pgms += "run-qc-list-dict-v8"; }
+    if ($pgmQcPickWords) { $pgms += "run-qc-dict-pick-qc-words"; }
+    if ($pgmQcQueryDictV8) { $pgms += "run-qc-query-dict-v8"; }
+    if ($pgmConvDcb) { $pgms += "run-convert-one-dcb"; }
+    if ($pgmQcV10Tags) { $pgms += "run-qc-v10-tags"; }
+    if ($pgmQcListDictV10) { $pgms += "run-qc-list-dict-v10"; }
+    if ($pgmQcQueryDictV10) { $pgms += "run-qc-query-dict-v10"; }
+    if ($pgmQcCompareTags) { $pgms += "run-qc-compare-tags"; }
+    if ($pgmQcCompareDict) { $pgms += "run-qc-compare-dict"; }
+    if ($pgmSizesAll) { 
+        $pgms += "run-check-and-add-sizes-to-file-natives"; 
+        $pgms += "run-check-and-add-sizes-to-file-images"; 
+    }
+    return $pgms;
+}
+
+
+function Process-Cell($dbRow, $runEnv, $pgm, $type="status") {
+    # Inits
+    CF-Init-RunEnv-This-Row $runEnv $dbRow
+
+    # Inits
+    $bStr = $runEnv.bStr
+    $dbid = $dbRow.dbid
+    $dcbPfn = $dbRow.conv_dcb;
+    $dbStr = "{0:0000}" -f [int]$dbid
+
+    $script:rowHasError = $false
+    $script:rowStatusGood = $false
+    try {
+        # Calc status field and status file
+        $pgmStatFld = $CF_PGMS.$pgm[0];
+        $pgmStatFileStub = $CF_PGMS.$pgm[1];
+        if ($type -eq "status") {
+            $pgmStatusFile = "${bStr}_${dbStr}_${pgmStatFileStub}_STATUS.txt"
+            $pgmStatusFilePFN =  "$($runEnv.ProgramLogsDir)\$pgmStatusFile"
+        }
+        elseif ($type -eq "results") {
+            $pgmStatFld += "_results"
+            $pgmStatusFile = "${bStr}_${dbStr}_${pgmStatFileStub}.txt"
+            $pgmStatusFilePFN =  "$($runEnv.SearchResultsDir)\$pgmStatusFile"
+        }
+
+        # if NotJustErros, reset for all 
+        # else only if currently = STATUS_FAILED
+        write-host "DBID = $dbid  pgm=$pgm old stat=$($dbrow.$pgmStatFld)"
+        if ($NotJustErrors) {
+            write-host "clear it"
+            $dbRow.$pgmStatFld = $CF_STATUS_READY
+        }
+        else {
+            if ($dbRow.$pgmStatFld -eq $CF_STATUS_FAILED) {
+                write-host "clear it"
+                $dbRow.$pgmStatFld = $CF_STATUS_READY
+            }
+        }
+    }
+    catch {
+        CF-Write-Log $script:statusFilePFN "|ERROR|$($error[0])"
+        $script:rowHasError = $true
+    }
+    CF-Finish-Log $script:statusFilePFN 
+}
+
+# For each row, call Process-Cell to just that pgm for just that row
+# So Process-Cell is called  #rows x #pgms times
+function Main {
+    $runEnv = CF-Init-RunEnv $BatchID
+    CF-Log-To-Master-Log $runEnv.bstr "" "STATUS" "START"
+
+    try {
+        # set up @pgms
+        $pgms = Build-List-Of-Pgms
+
+        # For this program, use a simple log file in curr dir to capture errors
+        $script:statusFilePFN = "run-update-statuses-STATUS.txt"
+        CF-Initialize-Log $statusFilePFN 
+
+        # List of results file to remove b/c had bad statuses
+        # Once I'm confident in this list, can have the script do the remove
+        $script:resultsToRm = "result-files-for-removal.txt"
+        CF-Initialize-Log $resultsToRm
+
+        # Load driver file, if using
+        if ($DriverFile) {
+            CF-Load-Driver-File $DriverFile
+        }
+
+        $dcbRows = CF-Read-DB-File "DCBs" "BatchID" $BatchID
+
+        # snippet to put in error*/good* files
+        if ($FileStub) { $FileStub = "-${FileStub}" }
+
+        # Setup start/stop rows (assume user specifies as 1-based)
+        if ($startRow -eq $null) { $startRow = 1 }
+        if ($endRow -eq $null) { $endRow = $dcbRows.length } 
+        CF-Log-To-Master-Log $runEnv.bstr "" "STATUS" "Start row=$startRow  End row=$endRow"
+         
+        # Main loop
+        # Super inefficient, but easiest way to re-write
+        # Now, Process-Cell is really acting like a Process-Cell, b/c it's called 
+        #
+        foreach ($pgm in $pgms) {
+            # The log of the munged error lines from all the pgms we're looking at
+            # It will also go in the curr dir  
+            $script:collectedErrLog = "errors-$($runEnv.bstr)${FileStub}-${pgm}.txt"
+            CF-Initialize-Log $collectedErrLog
+            CF-Write-File $collectedErrLog "PGM | DB_ID | CLIENT_ID | DCB | Timestampt | Err Msg" 
+
+            # the Good log
+            $script:collectedGoodLog = "good-$($runEnv.bstr)${FileStub}-${pgm}.txt"
+            CF-Initialize-Log $collectedGoodLog
+            CF-Write-File $collectedGoodLog "PGM | DB_ID | CLIENT_ID | DCB | Timestamp" 
+
+            for ($i = ($startRow-1) ; $i -lt $endRow ; $i++) {
+                $row = $dcbRows[$i]
+                CF-Init-RunEnv-This-Row $runEnv $row
+
+                # Only process this row if it's in the right batch 
+                if ($row.batchid -ne $BatchID) {
+                    continue
+                }
+
+                # Check against driver file, if using
+                if ($DriverFile) {
+                    if (-not (CF-Is-DBID-in-Driver $row.dbid)) {
+                        continue
+                    }
+                }
+                Process-Cell $row $runEnv $pgm
+            }
+        }
+
+        # Write out whole DB every time in case stop before end of run
+        CF-Write-DB-File "DCBs" $dcbRows
+    }
+    catch {
+        $error[0] | format-list
+        CF-Log-To-Master-Log $runEnv.bstr "" "ERROR" "$($error[0])"
+    }
+
+}     
+
+Main
+
+
---
+++ run-qc-dict-pick-qc-words.ps1
@@ -0,0 +1,152 @@
+<#
+SYNOPSIS 
+
+.DESCRIPTION
+
+.PARAMETER Name
+
+.PARAMETER Extension
+
+.INPUTS
+None. You cannot pipe objects to this script
+
+.OUTPUTS
+
+.EXAMPLE
+One or more examples
+
+.EXAMPLE
+
+.LINK
+
+.LINK
+
+#>
+
+param(
+    $BatchID,
+    $startRow,
+    $endRow
+)
+
+. ((Split-Path $script:MyInvocation.MyCommand.Path) + "/libConversion.ps1")
+
+function IsAlpha4($word) {
+    return $word -match "^[A-Za-z]{4,}$"
+}
+
+function IsAlphaNum4() {
+    return $word -match "^[A-Za-z][A-Za-z0-9]{3,}$"
+}
+
+function AddToResFile () {
+    if (-not ($script:qcwords.containskey($word))) {
+        CF-Write-File $resFilePFN $word
+        $script:qcwords[$word] = ""
+        if ($script:qcwords.count -ge $numWanted) {
+            return $true
+        }
+        else { return $false }
+    }
+    else { return $false }
+}
+
+# Picks the words and writes tehm ot    
+function Pick-QC-Words ($dictListFile, $resFilePFN) {
+    $numWanted = 3
+    echo $null > $resFilePFN
+    # Get up to $numWanted words
+    # first, try to fill all with alpha >= 4 chars
+    # if that fails, get alpha + 3 or more alphanum
+    # else take what we can get except for tokens containing doublequotes
+    # File format is just one dictionary word per line
+    $words = get-content $dictListFile
+    $script:qcwords = @{}
+
+    # All alpha
+    foreach ($word in $words) {
+        if (IsAlpha4 $word) {
+            if (AddToResFile) { return }
+        }
+    }
+
+    # Alpha + alphanum
+    foreach ($word in $words) {
+        if (IsAlphaNum4 $word) {
+            if (AddToResFile) { return }
+        }
+    }
+
+    # Anything except with double-quotes
+    # NOTE: if Modify punctuation, double-quotes embedded in strings can 
+    # be in a index word, but I don't know how to search for it
+    # Eg  aaa"doublequote   will be in dictionary
+    #    but "aaa"doublequote"  and 'aaa"doubleqouote' 
+    #    both throw errors when searching
+    foreach ($word in $words) {
+        if (!($word -match '"')) {
+            if (AddToResFile) { return }
+        }
+    }
+}
+
+function Process-Row($dbRow, $runEnv) {
+    # Inits
+    # Inits
+    $bStr = $runEnv.bStr
+    $dbid = $dbRow.dbid
+    $dbStr = "{0:0000}" -f [int]$dbid
+
+    ($script:statusFilePFN, $resFilePFN) = CF-Init-RunEnv-This-Row2 $runEnv $dbRow
+    $script:rowHasError = $false
+
+    try {
+
+        # Calc results file for the dict-list
+        $v8FileStub = $CF_PGMS['run-qc-list-dict-v8'][1];
+        $v8ResFilePFN = "${bStr}_${dbStr}_${v8FileStub}.txt"
+        $v8ResFilePFN =  "$($runEnv.SearchResultsDir)\$v8ResFilePFN"
+
+        CF-Write-Progress $dbid ""
+        Pick-QC-Words $v8ResFilePFN $resFilePFN
+
+    }
+    catch {
+        CF-Write-Log $script:statusFilePFN "|ERROR|$($error[0])"
+        $script:rowHasError = $true
+    }
+    CF-Finish-Log $script:statusFilePFN 
+}
+
+function Main {
+    $startdate = $(get-date -format $CF_DateFormat)
+    $rowsCompared = 0
+    $runEnv = CF-Init-RunEnv $BatchID 
+    $dcbRows = CF-Read-DB-File "DCBs" "BatchID" $BatchID
+
+    # Setup start/stop rows (assume user specifies as 1-based)
+    if ($startRow -eq $null) { $startRow = 1 }
+    if ($endRow -eq $null) { $endRow = $dcbRows.length } 
+    CF-Log-To-Master-Log $runEnv.bstr "" "STATUS" "Start row=$startRow  End row=$endRow"
+     
+    # DCB Rows Loop
+    for ($i = ($startRow-1) ; $i -lt $endRow ; $i++) {
+        $row = $dcbRows[$i]
+        $arrPreReqs = @()
+        $arrPreReqs += $row.st_qc_list_dict_v8
+        if (CF-Skip-This-Row $runEnv $row $arrPreReqs) {
+            continue
+        }
+
+        Process-Row $row $runEnv 
+    }
+
+    CF-Log-To-Master-Log $runEnv.bstr "" "STATUS" "STOP  Start row=$startRow  End row=$endRow"
+    $endDate = $(get-date -format $CF_DateFormat)
+    write-host "*** Done: batch = $BatchID Start row=$startRow  End row=$endRow"
+    write-host "Start: $startDate"
+    write-host "End:   $endDate"
+}
+Main
+
+
